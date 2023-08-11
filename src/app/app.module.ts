5947a1380e40fecd82ad664d9d30ddb74de23061
Aaron <9aaronlee@gmail.com>
2023-08-11 16:02:41 -0600

    forms

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TdfComponent } from './tdf/tdf.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TdfComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tdf/tdf.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from "@angular/core";
+import {
+FormGroup, FormControl, FormBuilder, Validators} from "@angular/forms";
+import { avoidWord } from "./customValidation";
+import { avoidWord1 } from "./customValidation";
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrls: ['./tdf.component.css']
+})
+export class TdfComponent implements OnInit {
+
+  signupForm = this.formB.group({
+    username: ["Aaron",[Validators.required, avoidWord]],
+    password: ["",[avoidWord1(/password/)]],
+    confirmedpassword: [""]
+    });
+    
+    // signupForm = new FormGroup({
+    // username: new FormControl(""),
+    // password: new FormControl(""),
+    // confirmedpassword: new FormControl("")
+    // });
+  constructor(private formB: FormBuilder) {}
+  form!: FormGroup;
+  ngOnInit() {
+  this.form = new FormGroup({
+  male: new FormControl(false),
+  female: new FormControl(false),
+  birthCountry: new FormControl('')
+  });
+  this.form.get('male')!.valueChanges.subscribe(chosen => {
+  if (chosen) {
+  this.form.get('birthCountry')!.setValidators([Validators.required]);
+  } else {
+  this.form.get('birthCountry')!.setValidators(null);
+  }
+  this.form.get('birthCountry')!.updateValueAndValidity();
+  });
+  }
+  submit() {
+  if(!this.form.valid) {
+  console.log("invalid data"); }
+  else{
+  console.log("valid data");
+  }
+  }
+  onSubmit(){
+    if (!this.signupForm.valid){
+    console.log("invalid");
+    }
+    else{
+    console.log(this.signupForm.value);
+    }
+    }
+  }
